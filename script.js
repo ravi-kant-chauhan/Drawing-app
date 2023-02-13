b072103c23b53d7cabc05a4396d740d61398421d
Ravi <ravkchauhan1aip@gmail.com>
2023-02-13 22:58:19 +0530

    update

--- script.js
+++ script.js
@@ -33,5 +33,11 @@ canvas.addEventListener('mousemove', (e) => {
     if(isPressed) {
         const x2 = e.offsetX
         const y2 = e.offsetY
+
+        drawCircle(x2, y2)
+        drawLine(x, y, x2, y2)
+
+        x = x2
+        y = y2
     }
 })
